e9d32ab83232cff5cc55a4c294d8d5843771039b
Giovana Furlan <giovananelofurlan@gmail.com>
2022-02-25 16:01:27 -0300

    Finalizando

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,135 @@
-import Head from 'next/head'
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Avatar, Badge, Box, ChakraProvider, Heading, Image, LinkBox, LinkOverlay, Stat, Text, Wrap, WrapItem } from '@chakra-ui/react'
+import { AddIcon, StarIcon } from '@chakra-ui/icons'
 
-export default function Home() {
+function Home({ Component, pageProps }) {
+
+    const property = {
+      imageUrl: 'https://bit.ly/2Z4KKcF',
+      imageAlt: 'Rear view of modern home with pool',
+      beds: 3,
+      baths: 2,
+      title: 'Modern home in city center in the heart of historic Los Angeles',
+      formattedPrice: '$1,900.00',
+      reviewCount: 34,
+      rating: 4,
+    }
+
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
+    <ChakraProvider>
+      <Wrap>
+        <WrapItem>
+          <Avatar size='xl' name='Logo' src='https://raichu-uploads.s3.amazonaws.com/logo_americanas-com-loja-online_wPPyZK.png' />{' '}
+          <Text fontSize='3xl'>Americanas - Loja Online</Text>
+        </WrapItem>
+      </Wrap>
+      <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+      <Accordion defaultIndex={[0]} allowMultiple>
+        
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex='1' textAlign='left'>
+                Section 1 title
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+          <Box position="fixed">
+            <Stat>
+              <Image src=""/>
+              <StatLabel>Sent</StatLabel>
+              <StatNumber>345,670</StatNumber>
+              <StatHelpText>
+                <StatArrow type='increase' />
+                23.36%
+              </StatHelpText>
+            </Stat>
+            <Box display="flex">
+              <LinkBox as='article' maxW='sm' p='5' borderWidth='1px' rounded='md'>
+                <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
+                  13 days ago
+                </Box>
+                <Heading size='md' my='2'>
+                  <LinkOverlay href='#'>
+                    New Year, New Beginnings: Smashing Workshops & Audits
+                  </LinkOverlay>
+                </Heading>
+                <Text>
+                  Catch up on what’s been cookin’ at Smashing and explore some of the most
+                  popular community resources.
+                </Text>
+              </LinkBox>
+              <LinkBox as='article' maxW='sm' p='5' borderWidth='1px' rounded='md'>
+                <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
+                  13 days ago
+                </Box>
+                <Heading size='md' my='2'>
+                  <LinkOverlay href='#'>
+                    New Year, New Beginnings: Smashing Workshops & Audits
+                  </LinkOverlay>
+                </Heading>
+                <Text>
+                  Catch up on what’s been cookin’ at Smashing and explore some of the most
+                  popular community resources.
+                </Text>
+              </LinkBox>
+            </Box>
+          </Box>
+          <Box p='6'>
+        <Box display='flex' alignItems='baseline'>
+          <Badge borderRadius='full' px='2' colorScheme='teal'>
+            New
+          </Badge>
+          <Box
+            color='gray.500'
+            fontWeight='semibold'
+            letterSpacing='wide'
+            fontSize='xs'
+            textTransform='uppercase'
+            ml='2'
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            {property.beds} beds &bull; {property.baths} baths
+          </Box>
+        </Box>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Box
+          mt='1'
+          fontWeight='semibold'
+          as='h4'
+          lineHeight='tight'
+          isTruncated
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
+          {property.title}
+        </Box>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+        <Box>
+          {property.formattedPrice}
+          <Box as='span' color='gray.600' fontSize='sm'>
+            / wk
+          </Box>
+        </Box>
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+        <Box display='flex' mt='2' alignItems='center'>
+          {Array(5)
+            .fill('')
+            .map((_, i) => (
+              <StarIcon
+                key={i}
+                color={i < property.rating ? 'teal.500' : 'gray.300'}
+              />
+            ))}
+          <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+            {property.reviewCount} reviews
+          </Box>
+        </Box>
+      </Box>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </Box>
+    </ChakraProvider>
   )
 }
+
+export default Home
